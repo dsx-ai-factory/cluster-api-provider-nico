9260eec641abc72cd228b29128f0321f86d81865
Jay Cox <jcox@nvidia.com>
2026-09-09 11:52:27 -0500

    chore: remove test slop test
    Signed-off-by: Jay Cox <jcox@nvidia.com>

--- controllers/nicomachine_controller_test.go
+++ controllers/nicomachine_controller_test.go
@@ -5,7 +5,6 @@ package controllers
 
 import (
 	"context"
-	"testing"
 	"time"
 
 	"github.com/onsi/ginkgo/v2"
@@ -25,10 +24,6 @@ const (
 	timeout     = 60 * time.Second
 	testMachine = "nicomachine-1"
 )
-
-func TestControllerTestPolicyRejectsNewTests(t *testing.T) {
-	t.Helper()
-}
 
 func nicoMachineCaseSet(description, dirPrefix string, defineSteps func(*fixtures.Case, fixtures.CaseSet)) fixtures.CaseSet {
 	return fixtures.CaseSet{
