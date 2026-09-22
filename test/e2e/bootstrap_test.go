b550136129d907eda2dfa52f9b207c917eb56c34
Zach Madell <zmadell@nvidia.com>
2026-09-22 08:49:39 -0600

    fix(e2e): use current module path
    Signed-off-by: Zach Madell <zmadell@nvidia.com>

--- test/e2e/bootstrap_test.go
+++ test/e2e/bootstrap_test.go
@@ -16,7 +16,7 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
-	"github.com/NVIDIA/cluster-api-provider-nico/test/utils"
+	"github.com/dsx-ai-factory/cluster-api-provider-nico/test/utils"
 )
 
 const fakeDockerImage = "fake-nico-api-server"
